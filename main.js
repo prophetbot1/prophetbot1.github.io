ec5d80cb022c26941668fc4e04e559af909454e6
prophetbot1 <145781507+prophetbot1@users.noreply.github.com>
2024-08-04 21:21:15 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ let rightBox = document.querySelector(".right")
 let arrow = document.querySelector(".arrow")
 let main = document.querySelector(".main")
 let leftBox = document.querySelector(".left")
-
+window.onload = function(){ 
+    var audio = document.getElementById('music');
+        audio.pause();
+}
     function spread(){
         if (rightBox.classList.contains('open')) {  
             rightBox.classList.remove('open');  
@@ -24,7 +27,14 @@ let leftBox = document.querySelector(".left")
             else{
                 arrow.style.transform = 'rotate(180deg)'; 
             }
-            setTimeout(() => { main.style.display='block'; }, 100);
+            setTimeout(() => { main.style.display='block'; }, 150);
         }  
 
+    }
+    function waao(){
+        let audio = document.getElementById('music');
+        let diangun = document.querySelector(".waao")
+        audio.play();
+        diangun.style.display= 'block';
+        setTimeout(() => { diangun.style.display= 'none';}, 1500);
     }
